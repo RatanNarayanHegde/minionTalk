10286d31183ef7c074a22c7dfe4752e5f544b293
Ratan Narayan Hegde <ratanhegde08@gmail.com>
2021-06-09 16:29:41 +0530

    add click functionality

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 var input = document.querySelector("#input");
-console.log(input);
+var transBtn = document.querySelector("#btn-translate");
+var output = document.querySelector("#output");
+
+var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTranslatedUrl(text) {
+  var url = serverUrl + "?text=" + text;
+  return url;
+}
+
+function handleClickEvent() {
+  // console.log(input.value);
+  var url = getTranslatedUrl(input.value);
+  console.log(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => console.log(json.contents.text));
+}
+
+transBtn.addEventListener("click", handleClickEvent);
